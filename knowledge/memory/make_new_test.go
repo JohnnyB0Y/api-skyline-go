1f57c83d918c28d834d965bb6b30b7087fe4b929
JohnnyB0Y <ag860050872@163.com>
2021-06-30 11:54:00 +0800

    Add make new test.

--- knowledge/memory/make_new_test.go
+++ knowledge/memory/make_new_test.go
@@ -6,11 +6,18 @@ import (
 )
 
 func TestMakeNew(t *testing.T) {
-	m := make([]int, 2)
-	n := new([]int)
-	s := []int{}
+
 	a := [...]int{1, 2, 3}
 	a2 := [...]int{1, 2, 3}
+	v_map := map[int]int{}
+	v_map2 := map[string]int{}
+
+	m := make([]int, 2)
+	n := new([]int)
+
+	s := []int{}
+	s2 := a[0:1]
+
 	i := 10086
 	i2 := 10086
 	i3 := 113114
@@ -20,22 +27,27 @@ func TestMakeNew(t *testing.T) {
 	m2 := make([]int, 2)
 	n2 := new([]int)
 
-	t.Logf("make pointer: %p\n", &m)
-	t.Logf("new pointer: %p\n", n)
-	t.Logf("slice pointer: %p\n", &s)
 	t.Logf("array 1 pointer: %p\n", &a)
 	t.Logf("array 2 pointer: %p\n", &a2)
 
-	t.Logf("int 1 pointer: %p\n", &i)
-	t.Logf("int 2 pointer: %p\n", &i2)
-	t.Logf("int 3 pointer: %p\n", &i3)
+	t.Logf("map   1 pointer: %p\n", &v_map)
+	t.Logf("map   2 pointer: %p\n", &v_map2)
 
-	t.Logf("str 1 pointer: %p\n", &str)
-	t.Logf("str 2 pointer: %p\n", &str2)
-	t.Logf("str 3 pointer: %p\n", &str3)
+	t.Logf("make pointer:    %p\n", &m)
+	t.Logf("new pointer:     %p\n", n)
+	t.Logf("slice 1 pointer: %p\n", &s)
+	t.Logf("slice 2 pointer: %p\n", &s2)
 
-	t.Logf("make 2 pointer: %p\n", &m2)
-	t.Logf("new 2 pointer: %p\n", n2)
+	t.Logf("int 1 pointer:   %p\n", &i)
+	t.Logf("int 2 pointer:   %p\n", &i2)
+	t.Logf("int 3 pointer:   %p\n", &i3)
+
+	t.Logf("str 1 pointer:   %p\n", &str)
+	t.Logf("str 2 pointer:   %p\n", &str2)
+	t.Logf("str 3 pointer:   %p\n", &str3)
+
+	t.Logf("make 2 pointer:  %p\n", &m2)
+	t.Logf("new 2 pointer:   %p\n", n2)
 
 	// 算一下地址，看是在栈上还是堆上
 	pm := uintptr(unsafe.Pointer(&m))
