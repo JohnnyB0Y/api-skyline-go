ee05a106bcefe38bfe97de49091901089f4b7ef0
JohnnyB0Y <ag860050872@163.com>
2021-07-05 22:28:53 +0800

    goroutine test.

---
+++ knowledge/common_data_structures/goroutine_test.go
@@ -0,0 +1,18 @@
+//  goroutine_test.go
+//
+//
+//  Created by JohnnyB0Y on 2021/07/05.
+//  Copyright © 2021 JohnnyB0Y. All rights reserved.
+
+package commondatastructures_test
+
+// 知识补充
+/**
+线程上下文切换：3.5us
+协程上下文切换：120ns
+线程大约是协程的 30倍
+资料来源：https://zhuanlan.zhihu.com/p/80037638
+
+
+
+*/
