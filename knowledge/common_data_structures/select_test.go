b5e6dd8f8bc57a31e60241fd1a500e94f3c104dc
JohnnyB0Y <ag860050872@163.com>
2021-07-05 19:06:19 +0800

    select test.

--- knowledge/common_data_structures/select_test.go
+++ knowledge/common_data_structures/select_test.go
@@ -28,13 +28,37 @@ func SelectExample1() {
 func SelectExample2() {
 	c := make(chan int)
 
-	// 会阻塞
-	select {
-	case <-c:
-		fmt.Println("Reading c.")
-	case c <- 10:
-		fmt.Println("Writing c.")
+	for i := 0; i < 3; i++ {
+		go func() {
+			fmt.Println("I'm Writing chan.........")
+			c <- 11
+		}()
 	}
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			fmt.Println("I'm Reading chan.........")
+			d := <-c
+			fmt.Println(d)
+		}()
+	}
+
+	fmt.Println("Before select chan!!!!")
+
+	// 会阻塞
+	for {
+		select {
+		case <-c:
+			fmt.Println("Select reading chan ..........")
+		case c <- 10:
+			fmt.Println("Select writing chan ..........")
+			// default:
+			// 	fmt.Println("Select Default ..........")
+		}
+	}
+
+	// 直接阻塞当前
+	// select {}
 }
 
 func SelectExample3() {
@@ -63,13 +87,33 @@ func SelectExample4() {
 }
 
 func TestSelect(t *testing.T) {
-	SelectExample1()
-	// SelectExample2()
-	SelectExample3()
-	SelectExample4()
+	// SelectExample1()
+	SelectExample2()
+	// SelectExample3()
+	// SelectExample4()
 }
 
 /**
 运行测试命令
 go test select_test.go -v -cover
+*/
+
+/**
+select 语句，相当于 管道操作选择器；
+当管道可操作时，会随机选择一个来执行；
+当管道不可操作时，看有没有default选项，有就执行，没有就阻塞；
+
+1）如果操作的管道为 nil，由于 nil管道不可读不可写，在运行时会被忽略掉；
+2）从管道读取数据；
+	- 当管道有数据可读时，当前case执行并返回；
+	- 当管道没有数据可读，且管道已关闭，当前case执行并返回；
+	- 当管道没有数据可读，且管道未关闭，当前case会被阻塞；
+3）向管道写入数据；
+	- 当管道的待读队列没有goroutine 排队时，且没有数据缓冲区 或 数据缓冲区已满时，当前case会被阻塞；
+	- 当管道的待读队列没有goroutine 排队时，有数据缓冲区且未满时，当前case执行并返回；
+	- 当管道的待读队列有goroutine 排队时，当前case执行并返回；
+4）default 选项不会对管道进行操作。
+	- 当管道不能读或不能写时，当前case执行并返回；
+	- 当default case 存在时，select 语句永远不会被阻塞，一般在检测管道是否可用时会用到；
+
 */
