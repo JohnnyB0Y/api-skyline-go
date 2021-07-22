4e78134ade1f67dedb010da4b1eeb2a91c66e0d6
JohnnyB0Y <ag860050872@163.com>
2021-07-22 23:30:35 +0800

    go algorithms

--- knowledge/algorithms/sort/merge.go
+++ knowledge/algorithms/sort/merge.go
@@ -5,3 +5,46 @@
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
 package sort
+
+func MergeSort(nums []int) []int {
+	N := len(nums)
+	// 结束条件
+	if N < 2 { // 0, 1
+		return nums
+	}
+
+	// 分解
+	left := MergeSort(nums[:N/2])
+	right := MergeSort(nums[N/2:])
+	// 合并
+	return merge(left, right)
+}
+
+// 合并左右
+func merge(left, right []int) []int {
+
+	if len(left) < 1 || len(right) < 1 {
+		return []int{}
+	}
+	if len(left) < 1 {
+		return right
+	}
+	if len(right) < 1 {
+		return left
+	}
+
+	nums := make([]int, 0, len(left)+len(right))
+	l, r := 0, 0
+	for i := 0; i < len(left)+len(right); i++ {
+
+		if l >= len(left) || (r < len(right) && left[l] >= right[r]) { // 左边走完 或 左边比较大
+			nums = append(nums, right[r])
+			r++
+
+		} else if r >= len(right) || (l < len(left) && left[l] <= right[r]) { // 右边走完 或 右边比较大
+			nums = append(nums, left[l])
+			l++
+		}
+	}
+	return nums
+}
--- knowledge/algorithms/sort/sort_test.go
+++ knowledge/algorithms/sort/sort_test.go
@@ -30,3 +30,23 @@ func TestQuickSort(t *testing.T) {
 	// [-99 -89 -11 0 1 2 2 23 34 43 99 100]
 	fmt.Println("end", nums)
 }
+
+func TestMergeSort(t *testing.T) {
+	nums := []int{14, 13, 11, 18, 19, 24, 3, 5, 1}
+	fmt.Println("start:", nums)
+	nums = MergeSort(nums)
+	// [1 3 5 11 13 14 18 19 24]
+	fmt.Println("end", nums)
+
+	nums = []int{1, 2, 89, 43, 23, 11, 0, 34, 99, 100, 99, 2}
+	fmt.Println("start:", nums)
+	nums = MergeSort(nums)
+	// [0 1 2 2 11 23 34 43 89 99 99 100]
+	fmt.Println("end", nums)
+
+	nums = []int{1, 2, -89, 43, 23, -11, 0, 34, 99, 100, -99, 2}
+	fmt.Println("start:", nums)
+	nums = MergeSort(nums)
+	// [-99 -89 -11 0 1 2 2 23 34 43 99 100]
+	fmt.Println("end", nums)
+}
