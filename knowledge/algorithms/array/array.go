6cd5210eb6a77591737e2ca1d797f5e10cbea48a
JohnnyB0Y <ag860050872@163.com>
2021-07-25 02:21:11 +0800

    go algorithms

--- knowledge/algorithms/array/array.go
+++ knowledge/algorithms/array/array.go
@@ -68,3 +68,51 @@ func RemoveDuplicates2(nums []int) int {
 	}
 	return fill
 }
+
+/**
+买卖股票的最佳时机 II
+给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2zsx1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func MaxProfit(prices []int) int {
+	// 遇到低就买入，遇到最高就卖出
+	// 寻找下一个低点，循环
+	length := len(prices)
+	if length < 2 {
+		return 0
+	}
+	//  0 1 2 3 4 5
+	// [7,1,5,3,6,4]
+	buy, sale, profit := 0, 0, 0
+	for buy < length-1 {
+		// 当前价格高于下一个价格
+		if prices[buy] > prices[buy+1] {
+			buy++ // 继续找买入点
+			continue
+		}
+
+		// 走到这里，算是找到了买入点，开始找卖出点
+		sale = buy + 1
+		for sale < length {
+			if sale == length-1 || prices[sale] > prices[sale+1] { // sale 是最后一个 或 比下一个价格高 就不找了
+				profit += prices[sale] - prices[buy]
+				buy = sale + 1 // 下一轮
+				break
+			}
+			sale++
+		}
+	}
+
+	return profit
+}
