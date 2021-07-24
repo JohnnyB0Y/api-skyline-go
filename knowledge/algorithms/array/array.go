e585b52387e7820bd98198ffe337cba3eb4eecdd
JohnnyB0Y <ag860050872@163.com>
2021-07-25 05:01:13 +0800

    go algorithms

--- knowledge/algorithms/array/array.go
+++ knowledge/algorithms/array/array.go
@@ -116,3 +116,137 @@ func MaxProfit(prices []int) int {
 
 	return profit
 }
+
+/**
+旋转数组
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+
+
+进阶：
+
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func rotate(nums []int, k int) {
+	// k = 3
+	// [1,2,3,(4),5,6,7]
+	// 1 -> 4; 4 -> 7; 7 -> 3; 3 -> 6; 6 -> 2; 2 -> 5; 5 -> 1;
+	//
+	// [5,6,7,(1),2,3,4]
+
+	// k = 4
+	// [1,2,3,4,(5),6,7]
+	// 1 -> 5; 5 -> 2; 2 -> 6; 6 -> 3; 3 -> 7; 7 -> 4; 4 -> 1;
+	// [4,5,6,7,(1),2,3]
+
+	// k = 4
+	// [1,2,3,4,(5),6,7,8]
+	// 1 -> 5; 5 -> 1;
+	// 2 -> 6; 6 -> 2;
+	// 3 -> 7; 7 -> 3;
+	// 4 -> 8; 8 -> 4;
+	// [5,6,7,8,(1),2,3,4]
+
+	// k = 3
+	// [1,2,3,(4),5,6,7,8,9]
+	// 1 -> 4; 4 -> 7; 7 -> 1;
+	// 2 -> 5; 5 -> 8; 8 -> 2;
+	// 3 -> 6; 6 -> 9; 9 -> 3;
+	// [7,8,9,(1),2,3,4,5,6]
+
+	// k = 4
+	// [1]
+	// [1,2]
+
+	// k = 4 (k > len(arr)) ==> k = k % len(arr) ==> k = 1
+	// [1,2,3]
+	// 1 -> 2; 2 -> 3; 3 ->1;
+	// [3,1,2]
+
+	// k = 4
+	// [1,2,3,4,5,6]
+	// 1 -> 5; 5 -> 3; 3 -> 1;
+	// 2 -> 6; 6 -> 4; 4 -> 2;
+	// [3,4,5,6,1,2]
+	// [3,4,5,6,1,2]
+
+	// 倒着走
+	length := len(nums)
+
+	if length < k {
+		k = k % length
+	}
+
+	if k == 0 || length < 1 || length == k {
+		return
+	}
+
+	temp := 0
+	j := 0
+	count := 0
+	first := 0
+	if length%k == 0 {
+		// 整除
+		for i := 0; i < k; i++ {
+			j = length - k + i
+			temp = nums[i]
+			// nums[0] = nums[6]
+			// nums[1] = nums[7]
+			// nums[2] = nums[8]
+			nums[i] = nums[j]
+			for j > k+i {
+				// nums[6] = nums[3]
+				// nums[7] = nums[4]
+				// nums[8] = nums[5]
+				nums[j] = nums[j-k]
+				j -= k
+			}
+			// nums[3] = temp
+			// nums[4] = temp
+			// nums[5] = temp
+			nums[j] = temp
+		}
+	} else {
+		// 不整除
+		// k = 4
+		// [1,2,3,4,(5),6,7]
+		// 1 -> 5; 5 -> 2; 2 -> 6; 6 -> 3; 3 -> 7; 7 -> 4; 4 -> 1;
+		// [4,5,6,7,(1),2,3]
+		for count < length {
+			j = length - k + first
+			temp = nums[first]
+			// nums[0] = nums[3]
+			nums[first] = nums[j]
+			count += 1
+			for j != first {
+				next := j - k
+				if next < 0 {
+					next = length + next
+				}
+				// nums[3] = nums[6]
+				// nums[6] = nums[2]
+				// nums[2] = nums[5]
+				// nums[5] = nums[1]
+				// nums[1] = nums[4]
+				nums[j] = nums[next]
+				count += 1
+				if next == first { // 回到原点
+					// nums[4] = nums[0]
+					nums[j] = temp
+					break
+				}
+				j = next
+			}
+
+			count += 1
+			first += 1
+		}
+	}
+}
