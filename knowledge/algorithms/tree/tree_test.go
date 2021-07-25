3f279432adf1b0db4848d1573f6e7c689096b460
JohnnyB0Y <ag860050872@163.com>
2021-07-25 08:41:07 +0800

    go algorithms

--- knowledge/algorithms/tree/tree_test.go
+++ knowledge/algorithms/tree/tree_test.go
@@ -5,3 +5,24 @@
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
 package tree
+
+import "testing"
+
+func TestMaxDepth(t *testing.T) {
+	root := TreeNode{}
+	root.Val = 3
+
+	root.Left = &TreeNode{}
+	root.Left.Val = 9
+
+	root.Right = &TreeNode{}
+	root.Right.Val = 20
+
+	root.Right.Left = &TreeNode{}
+	root.Right.Left.Val = 15
+
+	root.Right.Right = &TreeNode{}
+	root.Right.Right.Val = 7
+
+	t.Log(maxDepth(&root))
+}
