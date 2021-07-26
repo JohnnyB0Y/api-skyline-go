f8f11cb9ca7b2d588d0b927514f8bc17f6f2c833
JohnnyB0Y <ag860050872@163.com>
2021-07-26 23:29:56 +0800

    go algorithms

--- knowledge/algorithms/dp/dp.go
+++ knowledge/algorithms/dp/dp.go
@@ -43,3 +43,30 @@ func maxProfit(prices []int) int {
 
 	return 0
 }
+
+/**
+最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+*/
+
+func maxSubArray(nums []int) int {
+
+	return 0
+}
+
+/**
+打家劫舍
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnq4km/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func rob(nums []int) int {
+
+	return 0
+}
