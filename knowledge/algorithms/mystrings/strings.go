aa3206a1421b456283e4e5495d74500c9403807f
JohnnyB0Y <ag860050872@163.com>
2021-07-27 07:53:16 +0800

    go algorithms

--- knowledge/algorithms/mystrings/strings.go
+++ knowledge/algorithms/mystrings/strings.go
@@ -159,3 +159,38 @@ func firstUniqChar2(s string) int {
 	}
 	return -1
 }
+
+/**
+有效的字母异位词
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn96us/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func isAnagram(s string, t string) bool {
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	chars := make([]int, 26)
+	for i := 0; i < len(s); i++ {
+		chars[int(s[i])-97] += 1
+	}
+	for i := 0; i < len(t); i++ {
+		chars[int(t[i])-97] -= 1
+	}
+
+	for i := 0; i < len(chars); i++ {
+		if chars[i] > 0 {
+			return false
+		}
+	}
+
+	return true
+}
