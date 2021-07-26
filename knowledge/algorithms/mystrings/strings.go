367fccef080db3b40c444d16f504d57573cadeb4
JohnnyB0Y <ag860050872@163.com>
2021-07-27 05:51:27 +0800

    go algorithms

--- knowledge/algorithms/mystrings/strings.go
+++ knowledge/algorithms/mystrings/strings.go
@@ -6,6 +6,11 @@
 
 package mystrings
 
+import (
+	"bytes"
+	"strconv"
+)
+
 /**
 反转字符串
 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
@@ -31,4 +36,48 @@ func reverseString(s []byte) {
 		left++
 		right--
 	}
+}
+
+/**
+整数反转
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+
+如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+
+假设环境不允许存储 64 位整数（有符号或无符号）。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnx13t/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func reverse(x int) int {
+
+	xStr := strconv.Itoa(x)
+	isMinus := false
+	if x < 0 {
+		isMinus = true
+		xStr = xStr[1:]
+	}
+
+	var buffer bytes.Buffer
+	for i := len(xStr) - 1; i >= 0; i-- {
+		buffer.WriteByte(xStr[i])
+	}
+
+	var newX64, err = strconv.ParseInt(buffer.String(), 10, 64)
+	if err != nil {
+		return 0
+	}
+
+	if newX64 != int64(int32(newX64)) { // 不接受 64 位
+		return 0
+	}
+
+	if isMinus {
+		return -int(newX64)
+	}
+
+	return int(newX64)
 }
