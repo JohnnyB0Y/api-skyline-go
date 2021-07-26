be47728ee6f734db794f664410e9472fe5898ea3
JohnnyB0Y <ag860050872@163.com>
2021-07-26 23:28:52 +0800

    go algorithms

--- knowledge/algorithms/dp/dp.go
+++ knowledge/algorithms/dp/dp.go
@@ -5,3 +5,41 @@
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
 package dp
+
+/**
+爬楼梯
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn854d/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func climbStairs(n int) int {
+
+	return 0
+}
+
+/**
+买卖股票的最佳时机
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn8fsh/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func maxProfit(prices []int) int {
+
+	return 0
+}
