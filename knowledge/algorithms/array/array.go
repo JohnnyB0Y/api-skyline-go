39309b3a899e99327dfd918c54097ddd5a4a5e6e
JohnnyB0Y <ag860050872@163.com>
2021-07-30 07:42:09 +0800

    go algorithms

--- knowledge/algorithms/array/array.go
+++ knowledge/algorithms/array/array.go
@@ -603,3 +603,51 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		}
 	}
 }
+
+/**
+第一个错误的版本
+你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnto1s/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func firstBadVersion(n int) int {
+	if n == 1 {
+		return n
+	}
+
+	// 1 是不是bad
+	if isBadVersion(1) {
+		return 1
+	}
+
+	left := 1
+	right := n
+	pivot := n / 2
+	// 5 4
+	for left < right-1 {
+		// pivot = 2, 3, 4
+		if isBadVersion(pivot) {
+			// right = 4
+			right = pivot
+			pivot = (pivot-left)/2 + left
+		} else {
+			// left = 2, 3
+			left = pivot
+			pivot = (right-pivot)/2 + pivot
+		}
+	}
+	return right
+}
+
+// 模拟而已
+func isBadVersion(version int) bool {
+	return true
+}
