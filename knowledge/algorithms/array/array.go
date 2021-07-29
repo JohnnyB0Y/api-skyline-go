46feee372fdfada9bbd9a172fce6810234469ddc
JohnnyB0Y <ag860050872@163.com>
2021-07-29 21:35:19 +0800

    go algorithms

--- knowledge/algorithms/array/array.go
+++ knowledge/algorithms/array/array.go
@@ -366,3 +366,22 @@ func moveZeroes(nums []int) {
 		}
 	}
 }
+
+/**
+两数之和
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2jrse/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+func twoSum(nums []int, target int) []int {
+
+}
