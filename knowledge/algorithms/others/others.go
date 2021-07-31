d4ee3a709470d96c3382f7dda87fc72e657bab56
JohnnyB0Y <ag860050872@163.com>
2021-07-31 11:29:43 +0800

    go algorithms

--- knowledge/algorithms/others/others.go
+++ knowledge/algorithms/others/others.go
@@ -62,3 +62,35 @@ func (this *MinStack) Top() int {
 func (this *MinStack) GetMin() int {
 	return this.arr[this.top][1]
 }
+
+/**
+位1的个数
+编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+
+提示：
+
+请注意，在某些语言（如 Java）中，没有无符号整数类型。在这种情况下，输入和输出都将被指定为有符号整数类型，并且不应影响您的实现，因为无论整数是有符号的还是无符号的，其内部的二进制表示形式都是相同的。
+在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在上面的 示例 3 中，输入表示有符号整数 -3。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn1m0i/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func hammingWeight(num uint32) int {
+	var count int8
+	num2 := num << 1
+	// 1110 1100 1000 0000 num2
+	// 0111 0110 0100 0000 num2>>1
+	// 1111 1110 1100 1000 num
+	// +1    +1   +1   +1  = 4个1
+	for num > 0 {
+		if num2>>1 != num {
+			count++
+		}
+		num = num << 1
+		num2 = num2 << 1
+	}
+	return int(count)
+}
