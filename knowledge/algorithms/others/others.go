8b0a9a0606ab17d53fb6656732e2b7fb39ef366d
JohnnyB0Y <ag860050872@163.com>
2021-07-31 13:34:31 +0800

    go algorithms

--- knowledge/algorithms/others/others.go
+++ knowledge/algorithms/others/others.go
@@ -235,3 +235,37 @@ func isRight(left byte) bool {
 	}
 	return false
 }
+
+/**
+缺失数字
+给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+
+
+
+进阶：
+
+你能否实现线性时间复杂度、仅使用额外常数空间的算法解决此问题?
+
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnj4mt/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func missingNumber(nums []int) int {
+	len_N := len(nums)
+	if len_N == 1 {
+		if nums[0] == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	total := (1 + len_N) * len_N / 2
+	for i := 0; i < len_N; i++ {
+		total -= nums[i]
+	}
+	return total
+}
