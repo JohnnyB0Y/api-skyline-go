9481f83eec02bd6259fba3efee3c50af08dbe6d6
JohnnyB0Y <ag860050872@163.com>
2021-07-31 10:24:32 +0800

    go algorithms

--- knowledge/algorithms/linklist/linklist.go
+++ knowledge/algorithms/linklist/linklist.go
@@ -6,6 +6,11 @@
 
 package linklist
 
+import (
+	"math/rand"
+	"time"
+)
+
 /**
 合并两个有序链表
 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
@@ -81,4 +86,44 @@ func isPalindrome(head *ListNode) bool {
 	}
 
 	return true
+}
+
+/**
+环形链表
+给定一个链表，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnwzei/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+func hasCycle2(head *ListNode) bool {
+	track := map[*ListNode]bool{}
+	rand.Seed(time.Now().UnixNano())
+
+	for head != nil {
+		if node := track[head]; node { // 查水表
+			return true
+		}
+		track[head] = true // 记录
+
+		for i := 0; i < rand.Intn(6)+1; i++ { // 随机走几步
+			if head != nil {
+				head = head.Next
+			} else {
+				return false
+			}
+		}
+	}
+	return false
 }
