f0959154d85b699b36e9f7cb6dfcd30571dfb3cf
JohnnyB0Y <ag860050872@163.com>
2021-07-31 12:02:34 +0800

    go algorithms

--- knowledge/algorithms/others/others.go
+++ knowledge/algorithms/others/others.go
@@ -94,3 +94,22 @@ func hammingWeight(num uint32) int {
 	}
 	return int(count)
 }
+
+/**
+汉明距离
+两个整数之间的 汉明距离 指的是这两个数字对应二进制位不同的位置的数目。
+
+给你两个整数 x 和 y，计算并返回它们之间的汉明距离。
+*/
+
+func hammingDistance(x int, y int) int {
+	var count int
+	num := x ^ y
+	for num > 0 {
+		if num&1 == 1 {
+			count++
+		}
+		num = num >> 1
+	}
+	return count
+}
